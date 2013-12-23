6dad89fae6c6f1fbc9c574919fb70fb44246057a
Ryan Floyd <ryanfloyd@thezebra.com>
2013-12-23 14:45:23 -0600

    [#62607846] Added intelligence to select right Mongodb server depending on environment

--- settings.py
+++ settings.py
@@ -1,7 +1,30 @@
-SERVER_NAME = '127.0.0.1:8100'
-DOMAIN = {
-    'test': {},
-}
+import os
+
+# Determine if this is a dev, staging or prod env
+if not os.environ.get('EC2-ENV'):
+	SERVER_NAME = '127.0.0.1:8100'
+	
+	MONGO_HOST = 'localhost'
+	MONGO_PORT = 27017
+	MONGO_USERNAME = ''
+	MONGO_PASSWORD = ''
+	MONGO_DBNAME = 'zoo'
+elif os.environ['EC2-ENV'] == 'STAGING':
+	SERVER_NAME = 'zoo-staging.servers.thezebra.com'
+
+	MONGO_HOST = 'db-server'
+	MONGO_PORT = 27017
+	MONGO_USERNAME = ''
+	MONGO_PASSWORD = ''
+	MONGO_DBNAME = 'zoo'	
+elif os.environ['EC2-ENV'] == 'PROD':
+	SERVER_NAME = 'zoo.thezebra.com'
+
+	MONGO_HOST = 'db-server'
+	MONGO_PORT = 27017
+	MONGO_USERNAME = ''
+	MONGO_PASSWORD = ''
+	MONGO_DBNAME = 'zoo'
 
 # Enable reads (GET), inserts (POST) and DELETE for resources/collections
 # (if you omit this line, the API will default to ['GET'] and provide
@@ -12,11 +35,25 @@ RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
 # individual items  (defaults to read-only item access).
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 
+test = {
+	'schema': {
+		'objectID': {
+			'type': 'string',
+			'minlength': 48,
+			'maxlength': 48,
+			'required': True,
+			'unique': True,
+		},
+		'last_modified': {
+			'type': 'datetime',
+		}
+	}
+}
 
-# Please note that MONGO_HOST and MONGO_PORT could very well be left
-# out as they already default to a bare bones local 'mongod' instance.
-MONGO_HOST = 'localhost'
-MONGO_PORT = 27017
-MONGO_USERNAME = ''
-MONGO_PASSWORD = ''
-MONGO_DBNAME = 'zoo'
+DOMAIN = {
+    'test': test,
+}
+
+
+
+
